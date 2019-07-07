d01427039b7dd58b74e1d651fe2ddfc5f538c274
yotani19 <52497095+yotani19@users.noreply.github.com>
2019-07-07 21:45:51 +0900

    mask_functionの適用

--- pipline2.py
+++ pipline2.py
@@ -6,6 +6,7 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets.folder import pil_loader
 import pydicom
+from mask_function import load_mask
 from PIL import Image
 
 class SIIMDataset(Dataset):
@@ -27,14 +28,19 @@ class SIIMDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.df.loc[idx][0]
         image = pydicom.read_file(img_path).pixel_array
+        shape = np.array(image).shape
         image = Image.fromarray(image)
-        label = self.df.loc[idx][1] if self.phase == "train" else None
-        
+        label = None
+        if self.phase == "train"
+            label = self.df.loc[idx][1]
+            label = load_mask(shape[0],shape[1],label)
         #image = image.convert("RGB")
 
         if self.transform:
             image = self.transform(image)   # transform image to tensor
-        image = torch.stack(image)
+            if self.phase == "train":
+                label = self.transform(label)
+        #image = torch.stack(image)
         sample = {'image':image, 'label':label}
         return sample
 
@@ -45,8 +51,8 @@ def get_dataloaders(data, batch_size=8, study_level=False):
     data_transforms = {
         'train': transforms.Compose([
                 #transforms.Resize((224, 224)),
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomRotation(10),
+                #transforms.RandomHorizontalFlip(),
+                #transforms.RandomRotation(10),
                 transforms.ToTensor(),
                 #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) 
         ]),
